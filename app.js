9e03677e004a811dced4976fbbb35eecb93e9894
sumit2001 <sgsumitgupta17@gmail.com>
2022-02-07 14:02:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express=require("express");
+
+const app=express();
+
+app.use(express.static("public"))
+
+// let port=process.env.PORT || 5000;
+let port=5000;
+app.listen(port,()=>{
+    console.log("Listening to port "+port);
+})
