db3ec24dd037aca7fb991947ff4dc6518f46ffcd
sumit2001 <sgsumitgupta17@gmail.com>
2022-02-07 14:22:52 +0530

    commit

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app=express();
 
 app.use(express.static("public"))
 
-// let port=process.env.PORT || 5000;
-let port=5000;
+let port=process.env.PORT || 5000;
+// let port=5000;
 app.listen(port,()=>{
     console.log("Listening to port "+port);
 })
